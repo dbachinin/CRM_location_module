3fa8f4ad54351ef42b1ebb46d97f65dc40c3d1b6
Dmitry Bachinin <dbachinin@gmail.com>
2019-12-24 16:19:18 +0300

    generate merchant

---
+++ db/migrate/20191224131704_create_merchants.rb
@@ -0,0 +1,10 @@
+class CreateMerchants < ActiveRecord::Migration[6.0]
+  def change
+    create_table :merchants do |t|
+      t.string :name
+      t.string :location
+
+      t.timestamps
+    end
+  end
+end
